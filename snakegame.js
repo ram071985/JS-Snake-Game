72b364adc920452b74730397bd6807e43c676eed
ram071985 <reidmuchow@gmail.com>
2020-01-11 16:36:39 -0600

    added red rectangle 2d graphics inside of the canvas.

--- snakegame.js
+++ snakegame.js
@@ -5,9 +5,14 @@ let canvas;
 let canvasContext;
 
 
-window.onload = function() {
+    window.onload = function() {
      canvas = document.getElementById('gameCanvas');
      canvasContext = canvas.getContext('2d');
      canvasContext.fillStyle = 'grey';
      canvasContext.fillRect(0,0,canvas.width,canvas.height);
+     canvasContext.fillStyle = "red";
+     canvasContext.fillRect(100,200,50,25);
+     canvasContext.fillStyle = "red";
+     canvasContext.fillRect(150,200,50,25);
+     
 }
