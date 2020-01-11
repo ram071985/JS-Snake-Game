443badc1dddf5fd938e874b600c769190313a7c4
ram071985 <reidmuchow@gmail.com>
2020-01-11 16:46:19 -0600

    put draw items inside of function.

--- snakegame.js
+++ snakegame.js
@@ -1,18 +1,17 @@
-
-
-
 let canvas;
 let canvasContext;
 
+window.onload = function() {
+  canvas = document.getElementById("gameCanvas");
+  canvasContext = canvas.getContext("2d");
+  drawEverything();
+};
 
-    window.onload = function() {
-     canvas = document.getElementById('gameCanvas');
-     canvasContext = canvas.getContext('2d');
-     canvasContext.fillStyle = 'grey';
-     canvasContext.fillRect(0,0,canvas.width,canvas.height);
-     canvasContext.fillStyle = "red";
-     canvasContext.fillRect(100,200,50,25);
-     canvasContext.fillStyle = "red";
-     canvasContext.fillRect(150,200,50,25);
-     
+function drawEverything() {
+  canvasContext.fillStyle = "grey";
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  canvasContext.fillStyle = "red";
+  canvasContext.fillRect(100, 200, 50, 25);
+  canvasContext.fillStyle = "red";
+  canvasContext.fillRect(150, 200, 50, 25);
 }
