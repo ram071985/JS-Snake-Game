277521b438617ec5719fe9c0c750b4a52bee87f9
ram071985 <reidmuchow@gmail.com>
2020-01-11 16:21:57 -0600

    added css to html, changed canvas color to grey, added div container for canvas to see if that would make css easier to configure.

--- snakegame.js
+++ snakegame.js
@@ -8,6 +8,6 @@ let canvasContext;
 window.onload = function() {
      canvas = document.getElementById('gameCanvas');
      canvasContext = canvas.getContext('2d');
-     canvasContext.fillStyle = 'black';
+     canvasContext.fillStyle = 'grey';
      canvasContext.fillRect(0,0,canvas.width,canvas.height);
 }
