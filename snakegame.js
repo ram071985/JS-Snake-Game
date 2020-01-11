e3e1eb16460904271f6373a95bcc19e819deebde
ram071985 <reidmuchow@gmail.com>
2020-01-11 17:09:47 -0600

    added motion to canvas.

--- snakegame.js
+++ snakegame.js
@@ -1,17 +1,23 @@
 let canvas;
 let canvasContext;
+let appleX = 50;
 
 window.onload = function() {
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext("2d");
+
+  setInterval(drawEverything, 1000)
   drawEverything();
-};
+
+}
 
 function drawEverything() {
+    appleX = appleX + 20;
+    console.log(appleX);
   canvasContext.fillStyle = "grey";
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
   canvasContext.fillStyle = "red";
   canvasContext.fillRect(100, 200, 50, 25);
   canvasContext.fillStyle = "red";
-  canvasContext.fillRect(150, 200, 50, 25);
+  canvasContext.fillRect(75, 200, appleX, 25);
 }
