a7dde18cc43411fc06db1184dbdee4e259712542
ram071985 <reidmuchow@gmail.com>
2020-01-11 14:48:53 -0600

    fixed javascript DOM id spelling issue.

--- snakegame.js
+++ snakegame.js
@@ -6,7 +6,7 @@ let canvasContext;
 
 
 window.onload = function() {
-     canvas = documnet.getElementById('gameCanvas');
+     canvas = document.getElementById('gameCanvas');
      canvasContext = canvas.getContext('2d');
      canvasContext.fillStyle = 'black';
      canvasContext.fillRect(0,0,canvas.width,canvas.height);
