3ab97aec7ee6761eeba924e6016c65a84a022be8
ram071985 <reidmuchow@gmail.com>
2020-01-13 17:01:50 -0600

    added new functions for movement and setInterval

--- snakegame.js
+++ snakegame.js
@@ -1,23 +1,31 @@
 let canvas;
 let canvasContext;
 let appleX = 50;
+let appleSpeedX = 5;
 
 window.onload = function() {
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext("2d");
 
-  setInterval(drawEverything, 1000)
-  drawEverything();
+  let framesPerSecond = 30;
+  setInterval(function () {
+    moveEveryThing();
+    drawEverything();
+  }, 1000/framesPerSecond);
 
 }
-
+function moveEveryThing() {
+  appleX = appleX + appleSpeedX;
+  if(appleX > 800) {
+    appleSpeedX = -5;
+  } else if(appleX < 0) {
+    appleSpeedX = 5;
+  }
+}
 function drawEverything() {
-    appleX = appleX + 20;
-    console.log(appleX);
+  moveEveryThing();
   canvasContext.fillStyle = "grey";
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
   canvasContext.fillStyle = "red";
-  canvasContext.fillRect(100, 200, 50, 25);
-  canvasContext.fillStyle = "red";
-  canvasContext.fillRect(75, 200, appleX, 25);
+  canvasContext.fillRect(appleX, 100, 10,10 );
 }
