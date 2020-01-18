c0e49085e6753bf93fab9763a4890cd41e213331
ram071985 <reidmuchow@gmail.com>
2020-01-18 13:18:16 -0600

    added landing page game canvas

--- snakegame.js
+++ snakegame.js
@@ -7,13 +7,12 @@ let snakeGrowX = snakeX - 23;
 window.onload = function() {
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext("2d");
+  drawEverything();
 
-
-  let framesPerSecond = 30;
-  setInterval(function() {
-    moveSnake();
-    drawEverything();
-  }, 1000 / framesPerSecond);
+  //let framesPerSecond = 30;
+ // setInterval(function() {
+  //  moveSnake();
+ // }, 1000 / framesPerSecond);
 };
 
 function moveSnake() {
@@ -34,25 +33,29 @@ function drawEverything() {
   canvasContext.fillRect(0,0,800, 45);
   canvasContext.fillStyle = "red";
   canvasContext.beginPath();
-  canvasContext.arc(20, 22.5, 14, 0, Math.PI * 2, true);
+  canvasContext.arc(20, 22.5, 10, 0, Math.PI * 2, true);
   canvasContext.fill();
-  canvasContext.font = "34px Times New Roman";
+  canvasContext.font = "30px Times New Roman";
   canvasContext.fillStyle = "white";
   canvasContext.fillText("=",40,34);
   canvasContext.fillStyle = "green";
   canvasContext.beginPath();
-  canvasContext.arc(snakeX, 100, 12, 0, Math.PI * 2, true);
+  canvasContext.arc(200, 300, 12, 0, Math.PI * 2, true);
   canvasContext.fill();
   canvasContext.fillStyle = "green";
   canvasContext.beginPath();
-  canvasContext.arc(snakeGrowX, 100, 12, 0, Math.PI * 2, true);
+  canvasContext.arc(177, 300, 12, 0, Math.PI * 2, true);
+  canvasContext.fill();
+  canvasContext.fillStyle = "red";
+  canvasContext.beginPath();
+  canvasContext.arc(623, 300, 10, 0, Math.PI * 2, true);
   canvasContext.fill();
 
  function loadScore() {
 
-    canvasContext.font = "34px Times New Roman";
+    canvasContext.font = "28px Times New Roman";
     canvasContext.fillStyle = "white";
-    canvasContext.fillText("0",67,34);
+    canvasContext.fillText("0",63,34);
   }
 loadScore();
 }
