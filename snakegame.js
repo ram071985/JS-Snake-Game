c821194a1ee6f2f66a75a59315c83ec40e4bec83
ram071985 <reidmuchow@gmail.com>
2020-01-22 16:04:53 -0600

    changed setInterval

--- snakegame.js
+++ snakegame.js
@@ -29,10 +29,6 @@ function drawStartGame() {
   canvasContext.beginPath();
   canvasContext.arc(200, 300, 12, 0, Math.PI * 2, true);
   canvasContext.fill();
-  // canvasContext.fillStyle = "green";
-  // canvasContext.beginPath();
-  // canvasContext.arc(177, 300, 12, 0, Math.PI * 2, true);
-  canvasContext.fill();
   canvasContext.fillStyle = "red";
   canvasContext.beginPath();
   canvasContext.arc(623, 300, 10, 0, Math.PI * 2, true);
@@ -45,77 +41,34 @@ function drawStartGame() {
   }
   loadScore();
 }
-function xAxisMoveRight() {
-  snakeX = snakeX + snakeSpeedX;
-  if (snakeX === 790) {
-    snakeSpeedX = 0;
-  }
-}
 
-function yAxisMoveDown() {
-  snakeY = snakeY - snakeSpeedY;
-  if (snakeY === 10) {
-    snakeSpeedY = 0;
-  }
-}
-
-
-  window.addEventListener("keydown", function(event) {
-    let framesPerSecond = 30;
-setInterval(function() {
-  if (event.code === "ArrowRight") {
-    snakeY = null;
-    moveRight();
-    xAxisMoveRight();
-  } else if (event.code === "ArrowDown") {
-    snakeX = null;
-   moveDown();
-   yAxisMoveDown();
-  };
-}, 1000 / framesPerSecond);
-  });
-
-
-function moveRight() {
-  canvasContext.fillStyle = "black";
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-  canvasContext.fillStyle = "green";
-  canvasContext.fillRect(0, 0, 800, 45);
-  canvasContext.fillStyle = "red";
-  canvasContext.beginPath();
-  canvasContext.arc(20, 22.5, 10, 0, Math.PI * 2, true);
-  canvasContext.fill();
-  canvasContext.font = "30px Times New Roman";
-  canvasContext.fillStyle = "white";
-  canvasContext.fillText("=", 40, 34);
-  canvasContext.fillStyle = "green";
-  canvasContext.beginPath();
-  canvasContext.arc(snakeX, 300, 12, 0, Math.PI * 2, true);
-  canvasContext.fill();
-  canvasContext.fillStyle = "red";
-  canvasContext.beginPath();
-  canvasContext.arc(623, 300, 10, 0, Math.PI * 2, true);
-  canvasContext.fill();
-}
-
-function moveDown() {
-  canvasContext.fillStyle = "black";
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-  canvasContext.fillStyle = "green";
-  canvasContext.fillRect(0, 0, 800, 45);
-  canvasContext.fillStyle = "red";
-  canvasContext.beginPath();
-  canvasContext.arc(20, 22.5, 10, 0, Math.PI * 2, true);
-  canvasContext.fill();
-  canvasContext.font = "30px Times New Roman";
-  canvasContext.fillStyle = "white";
-  canvasContext.fillText("=", 40, 34);
-  canvasContext.fillStyle = "green";
-  canvasContext.beginPath();
-  canvasContext.arc(200, snakeY, 12, 0, Math.PI * 2, true);
-  canvasContext.fill();
-  canvasContext.fillStyle = "red";
-  canvasContext.beginPath();
-  canvasContext.arc(623, 300, 10, 0, Math.PI * 2, true);
-  canvasContext.fill();
-}
+window.addEventListener("keydown", function(event) {
+  let moveRight = snakeX + snakeSpeedX;
+  let moveLeft = snakeX - snakeSpeedX;
+  let moveUp = snakeY + snakeSpeedY;
+  let moveDown = snakeY - snakeSpeedY;
+  let framesPerSecond = 30;
+  setInterval(function() {
+    if (event.code === "ArrowUp") {
+      canvasContext.fillStyle = "green";
+      canvasContext.beginPath();
+      canvasContext.arc(200, moveUp, 12, 0, Math.PI * 2, true);
+      canvasContext.fill();
+    } else if (event.code === "ArrowDown") {
+      canvasContext.fillStyle = "green";
+      canvasContext.beginPath();
+      canvasContext.arc(200, moveDown, 12, 0, Math.PI * 2, true);
+      canvasContext.fill();
+    } else if (event.code === "ArrowRight") {
+      canvasContext.fillStyle = "green";
+      canvasContext.beginPath();
+      canvasContext.arc(moveRight, 300, 12, 0, Math.PI * 2, true);
+      canvasContext.fill();
+    } else if (event.code === "ArrowLeft") {
+      canvasContext.fillStyle = "green";
+      canvasContext.beginPath();
+      canvasContext.arc(moveLeft, 300, 12, 0, Math.PI * 2, true);
+      canvasContext.fill();
+    }
+  }, 1000 / framesPerSecond);
+});
