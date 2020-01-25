f4089c3c88c8e6987942cefaca71a7c90064bbd3
ram071985 <reidmuchow@gmail.com>
2020-01-24 18:03:28 -0600

    remade functions and started over.

--- snakegame.js
+++ snakegame.js
@@ -1,109 +1,94 @@
-let canvas;
-let canvasContext;
-let snakeX = 200;
+// GOAL 1: move the snake to the right when the app starts
+// GOAL 2: move the snake down when the app starts
+
+// REQUIREMENTS: use the snakeSpeedY and snakeSpeedX to accomplish your goals
+
+let snakeX = 60;
+let snakeY = 60;
 let snakeSpeedX = 5;
-// let snakeGrowX = snakeX - 23;
-let snakeY = null;
-let snakeSpeedY = null;
-let moveUp = null;
-let moveDown = null;
-let moveRight = null;
-let moveLeft = null;
-// let snakeGrowY = snake
+let snakeSpeedY = 5;
+let snakeDirection;
 
-window.onload = function () {
-  canvas = document.getElementById("gameCanvas");
-  canvasContext = canvas.getContext("2d");
-  let framesPerSecond = 30;
-  setInterval(function () {
-    drawGame();
-    if (moveUp > 0) {
-      console.log(moveUp > 0);
-      canvasContext.fillStyle = "green";
-      canvasContext.beginPath();
-      canvasContext.arc(200, moveUp, 12, 0, Math.PI * 2, true);
-      canvasContext.fill();
-    } else if (moveDown === true) {
-      canvasContext.fillStyle = "green";
-      canvasContext.beginPath();
-      canvasContext.arc(200, moveDown, 12, 0, Math.PI * 2, true);
-      canvasContext.fill();
-    } else if (moveRight === true) {
-      canvasContext.fillStyle = "green";
-      canvasContext.beginPath();
-      canvasContext.arc(moveRight, 300, 12, 0, Math.PI * 2, true);
-      canvasContext.fill();
-    } else if (moveLeft === true) {
-      canvasContext.fillStyle = "green";
-      canvasContext.beginPath();
-      canvasContext.arc(moveLeft, 300, 12, 0, Math.PI * 2, true);
-      canvasContext.fill();
-    }
-  }, 1000 / framesPerSecond);
-};
-
-function drawGame() {
+function drawCanvas() {
   canvasContext.fillStyle = "black";
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-  canvasContext.fillStyle = "green";
-  canvasContext.fillRect(0, 0, 800, 45);
-  canvasContext.fillStyle = "red";
-  canvasContext.beginPath();
-  canvasContext.arc(20, 22.5, 10, 0, Math.PI * 2, true);
-  canvasContext.fill();
-  canvasContext.font = "30px Times New Roman";
-  canvasContext.fillStyle = "white";
-  canvasContext.fillText("=", 40, 34);
-  canvasContext.fillStyle = "green";
-  canvasContext.beginPath();
-  canvasContext.arc(200, 300, 12, 0, Math.PI * 2, true);
-  canvasContext.fill();
-  canvasContext.fillStyle = "red";
-  canvasContext.beginPath();
-  canvasContext.arc(623, 300, 10, 0, Math.PI * 2, true);
-  canvasContext.fill();
-
-  //function loadScore() {
-  // canvasContext.font = "28px Times New Roman";
-  // canvasContext.fillStyle = "white";
-  // canvasContext.fillText("0", 63, 34);
-  // }
-  //  loadScore();
 }
 
-function snakeMoveDown() {
-  moveDown = snakeY - snakeSpeedY;
+function snakeStartPosition() {
   canvasContext.fillStyle = "green";
   canvasContext.beginPath();
-  canvasContext.arc(200, moveDown, 12, 0, Math.PI * 2, true);
+  canvasContext.arc(60, 60, 12, 0, Math.PI * 2, true);
   canvasContext.fill();
 }
 
-function snakeMoveRight() {
-  moveRight = snakeX + snakeSpeedX;
+function moveRight() {
   canvasContext.fillStyle = "green";
   canvasContext.beginPath();
-  canvasContext.arc(moveRight, 300, 12, 0, Math.PI * 2, true);
+  canvasContext.arc(snakeX, 60, 12, 0, Math.PI * 2, true);
   canvasContext.fill();
 }
 
-function snakeMoveLeft() {
-  moveRight = snakeX - snakeSpeedX;
+function moveLeft() {
   canvasContext.fillStyle = "green";
   canvasContext.beginPath();
-  canvasContext.arc(200, moveDown, 12, 0, Math.PI * 2, true);
+  canvasContext.arc(snakeX, 60, 12, 0, Math.PI * 2, true);
   canvasContext.fill();
 }
-window.addEventListener("keydown", function (event) {
-  if (event.code === "ArrowUp") {
-    snakeMoveUp();
-  } else if (event.code === "ArrowDown") {
-  } else if (event.code === "ArrowRight") {
-  } else if (event.code === "ArrowLeft") {
-  }
 
-  function snakeMoveUp() {
-    moveUp = snakeY + snakeSpeedY;
-  
+function moveUp() {
+  canvasContext.fillStyle = "green";
+  canvasContext.beginPath();
+  canvasContext.arc(60, snakeY, 12, 0, Math.PI * 2, true);
+  canvasContext.fill();
+}
+
+function moveDown() {
+  canvasContext.fillStyle = "green";
+  canvasContext.beginPath();
+  canvasContext.arc(60, snakeY, 12, 0, Math.PI * 2, true);
+  canvasContext.fill();
+}
+
+
+
+window.addEventListener("keydown", function(e) {
+  if (e.code === "ArrowRight") {
+    snakeDirection = "right";
+  } else if (e.code === "ArrowLeft") {
+    snakeDirection = "left"; 
+  } else if (e.code === "ArrowUp") {
+    snakeDirection = "up";
+  } else if (e.code === "ArrowDown") {
+    snakeDirection = "down";
   }
 });
+
+
+window.onload = function() {
+  canvas = document.getElementById("gameCanvas");
+  canvasContext = canvas.getContext("2d");
+
+  
+  setInterval(function() {
+    drawCanvas();
+    if (snakeDirection === "right") {
+    moveRight();
+    snakeX = snakeX + snakeSpeedX;
+    } else if (snakeDirection === "left") {
+      moveLeft(); 
+      snakeX = snakeX - snakeSpeedX;
+    } else if (snakeDirection === "up") {
+      moveUp();
+      snakeY = snakeY - snakeSpeedY;
+    } else if (snakeDirection === "down") {
+      snakeY = snakeY + snakeSpeedY;
+     moveDown();
+    } else {
+      snakeStartPosition();
+    }
+  }, 1000);
+ 
+};
+
+
+// }
