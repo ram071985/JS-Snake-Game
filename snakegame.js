3dffdad2217fdd9089b66160a2bcfbf156f87502
ram071985 <reidmuchow@gmail.com>
2020-02-06 14:46:33 -0600

    added score functions.

--- snakegame.js
+++ snakegame.js
@@ -10,79 +10,83 @@ let fruitY = Math.floor(Math.random() * 30) * 20;
 function drawCanvas() {
   canvasContext.fillStyle = "black";
   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
 }
 
 function drawSnake() {
   for (i = 0; i < snakeArray.length; i++) {
-    if (snakeDirection === up) {
-   let currentItem = snakeArray.slice(-1)[0]
     canvasContext.fillStyle = "green";
-    canvasContext.fillRect(snakeArray[i].x - , snakeArray[i].y, 20, 20);
+    canvasContext.fillRect(snakeArray[i].x, snakeArray[i].y, 20, 20);
   }
 }
 
-//function moveUp() {
-// for (i = 0; i < snakeArray.length; i++) {
-//   if (snakeArray[i] !== 0)
-//snakeArray[i].y = ;
-//  }
-//}
+function moveSnake() {
+  const snakeCopy = snakeArray.map(snakePart => {
+    return { x: snakePart.x, y: snakePart.y };
+  });
+  if (snakeDirection === "right") {
+    console.log(snakeDirection === "right")
+    snakeArray[0].x += 20;
+  } else if (snakeDirection === "left") {
+    snakeArray[0].x -= 20;
+  } else if (snakeDirection === "up") {
+    snakeArray[0].y -= 20;
+  } else if (snakeDirection === "down") {
+    snakeArray[0].y += 20;
+  }
+  for (i = 1; i < snakeArray.length; i++) {
+    snakeArray[i].x = snakeCopy[i - 1].x;
+    snakeArray[i].y = snakeCopy[i - 1].y;
+  }
+}
 
-  function moveSnake() {
-    for (i = 0; i < snakeArray.length; i++) {
-      if (snakeDirection === "right") {
-        snakeArray[i].x = snakeArray[i].x + 20;
-      } else if (snakeDirection === "left") {
-      snakeArray[i].x = snakeArray[i].x - 20;
-      } else if (snakeDirection === "up") {
-      snakeArray[i].y = snakeArray[i].y - 20;
-      } else if (snakeDirection === "down") {
-      snakeArray[i].y = snakeArray[i].y + 20;
-      }
-    }
-    
-  } 
-
-  function addSnakePieceUp() {
-    for (i = 0; i < snakeArray.length; i++) {
-    if (snakeDirection === "up") {
-    let lastItem = snakeArray.slice(-2)[0];
-    let currentItem = snakeArray.slice(-1)[0]
-    console.log(currentItem)
+function addSnakePieceUp() {
+  if (snakeDirection === "up") {
     let newSnakePiece = {
       x: snakeArray[0].x,
-      y: lastItem.y - 20};
+      y: snakeArray[0].y + 20
+    };
     snakeArray.push(newSnakePiece);
-    }
   }
-  }
+}
 
-  function addSnakeDown() { 
-    if (snakeDirection === "down") {
-      let lastItem = snakeArray.slice(-2)[0];
-      let newSnakePiece = {
-        x: lastItem.x + 20,
-        y: snakeArray[0].y};
-      snakeArray.push(newSnakePiece);
-    }
+function addSnakePieceDown() {
+  if (snakeDirection === "down") {
+    let newSnakePiece = {
+      x: snakeArray[0].x,
+      y: snakeArray[0].y - 20
+    };
+    snakeArray.push(newSnakePiece);
   }
+}
 
-  function addSnakePieceRight() {
-    if (snakeDirection === "right") {
-      let lastItem = snakeArray.slice(-2)[0];
-      let newSnakePiece = {
-        x: lastItem.x + 20,
-        y: snakeArray[0].y};
-      snakeArray.push(newSnakePiece);
-    }
+function addSnakePieceRight() {
+  if (snakeDirection === "right") {
+    let newSnakePiece = {
+      x: snakeArray[0].x - 20,
+      y: snakeArray[0].y
+    };
+    snakeArray.push(newSnakePiece);
   }
+}
 
+function addSnakePieceLeft() {
+  if (snakeDirection === "left") {
+    let newSnakePiece = {
+      x: snakeArray[0].x + 20,
+      y: snakeArray[0].y
+    };
+    snakeArray.push(newSnakePiece);
+  }
+}
 function makeFruit() {
   if (snakeArray[0].x === fruitX && snakeArray[0].y === fruitY) {
     canvasContext.fillStyle = "red";
     canvasContext.fillRect(0, 0, 0, 0);
+    addSnakePieceUp();
+    addSnakePieceDown();
     addSnakePieceRight();
-    console.log(snakeArray);
+    addSnakePieceLeft();
     fruitX = Math.floor(Math.random() * 40) * 20;
     fruitY = Math.floor(Math.random() * 30) * 20;
   } else {
@@ -90,29 +94,87 @@ function makeFruit() {
     canvasContext.fillRect(fruitX, fruitY, 20, 20);
   }
 }
+/*function drawFruit() {
+  canvasContext.fillStyle = "red";
+  canvasContext.fillRect(fruitX, fruitY, 20, 20);
+} */
 
-window.addEventListener("keydown", function(e) {
-  if (e.code === "ArrowRight") {
-    snakeDirection = "right";
-  } else if (e.code === "ArrowLeft") {
-    snakeDirection = "left";
-  } else if (e.code === "ArrowUp") {
-    snakeDirection = "up";
-  } else if (e.code === "ArrowDown") {
-    snakeDirection = "down";
+function noScroll() {
+  window.scrollTo(0, 800);
+}
+
+function startOver() {
+  if (snakeArray[0].x === 800) {
+    window.alert("GAME OVER!");
+    window.location.reload(false);
+  } else if (snakeArray[0].x === -20) {
+    window.alert("GAME OVER!");
+    window.location.reload(false);
+  } else if (snakeArray[0].y === -20) {
+    window.alert("GAME OVER!");
+    window.location.reload(false);
+  } else if (snakeArray[0].y === 600) {
+    window.alert("GAME OVER!");
+    window.location.reload(false);
   }
+  for (i = 1; i < snakeArray.length; i++) {
+    if (
+      snakeArray[0].x === snakeArray[i].x &&
+      snakeArray[0].y === snakeArray[i].y
+    ) {
+      window.alert("GAME OVER!");
+      window.location.reload(false);
+    }
+  }
+}
+
+function addScore() {
+  let scoreNumber = document.getElementById("score");
+  let scoreText = scoreNumber.textContent;
+  if (snakeArray[0].x === fruitX && snakeArray[0].y === fruitY) {
+    let arrayLength = snakeArray.length;
+    let arrayLengthString = arrayLength.toString();
+    console.log(typeof arrayLengthString)
+    
+  } 
+}
+
+
+  window.addEventListener("keydown", function(e) {
+    if (e.code === "ArrowRight" && snakeDirection != "left") {
+      snakeDirection = "right";
+    } else if (e.code === "ArrowLeft" && snakeDirection != "right") {
+      snakeDirection = "left";
+    } else if (e.code === "ArrowUp" && snakeDirection != "down") {
+    snakeDirection = "up";
+   } else if (e.code === "ArrowDown" && snakeDirection != "up") {
+    snakeDirection = "down";
+   }
 });
+
+window.addEventListener("scroll", noScroll);
 
 window.onload = function() {
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext("2d");
-  let framesPerSecond = 5;
+  let framesPerSecond = 10;
   setInterval(function() {
     drawCanvas();
     makeFruit();
-    drawSnake();
+    /*   if (snakeArray[0].x === fruitX && snakeArray[0].y === fruitY) {
+      canvasContext.fillStyle = "red";
+      canvasContext.fillRect(0, 0, 0, 0);
+      addSnakePieceUp();
+      addSnakePieceRight();
+      console.log(snakeArray);
+      fruitX = Math.floor(Math.random() * 40) * 20;
+      fruitY = Math.floor(Math.random() * 30) * 20;
+    }*/
 
     moveSnake();
-    console.log(snakeArray);
+    addScore();
+    drawSnake();
+    startOver();
+    console.log(snakeArray[0])
   }, 1000 / framesPerSecond);
 };
